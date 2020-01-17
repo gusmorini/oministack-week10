de6cbd55d07e694762a44348d274182bf54fb090
Gustavo Morini <gustavommh@gmail.com>
2020-01-16 21:49:19 -0300

    estilo

--- web/src/App.js
+++ web/src/App.js
@@ -1,31 +1,23 @@
 import React, { useState } from 'react';
 
-// componente: bloco isolado de HTML, CSS e JS, o qual não interfere no restante da app
-// propriedade: Informações que o componente Pai passa para o componente Filho
-// estado: Informações mantidas pelo componente (lembrar: imutabilidade)
-
-// <> </> fragment para encapsular mais de um componente sem afetar a interface
-
-import Hello from './Hello';
+import './global.css';
+import './App.css';
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <>
-      <Hello title="titulo 2" />
-      <Hello title="titulo 1" />
-      <Hello title="titulo 3" />
+    <div id="app">
+      <aside>
+        <strong>cadastrar</strong>
+        <form>
+          <label htmlFor="">Usuário do Github</label>
+          <input name="github_username" id="username" />
+        </form>
+      </aside>
+      <main>
 
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>increment</button>
-
-    </>
+      </main>
+    </div>
   );
 }
 
