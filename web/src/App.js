236aea1cca3aeaf10a9da5a8abdd39fe914da185
Gustavo Morini <gustavommh@gmail.com>
2020-01-17 21:16:40 -0300

    web

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
@@ -7,31 +8,106 @@ import './Main.css';
 
 function App() {
 
+  const [devs, setDevs] = useState([]);
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    loadDevs();
+  });
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    });
+
+    setGithubUsername('');
+    setTechs('');
+
+    setDevs([...devs, response.data]);
+
+  }
+
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
 
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" />
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" />
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" />
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" />
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
@@ -42,41 +118,20 @@ function App() {
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/32224862?s=460&v=4" alt="Gustavo Morini" />
-              <div className="user-info">
-                <strong>Gustavo Morini</strong>
-                <span>React....</span>
-              </div>
-            </header>
-            <p>do que me falta não tenho nada do que me falta não tenho nada do que me falta não tenho nada do que me falta não tenho nada</p>
-            <a href="https://github.com/gusmorini">Perfil do Github</a>
-          </li>
 
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/32224862?s=460&v=4" alt="Gustavo Morini" />
-              <div className="user-info">
-                <strong>Gustavo Morini</strong>
-                <span>React....</span>
-              </div>
-            </header>
-            <p>bio ...</p>
-            <a href="https://github.com/gusmorini">Perfil do Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/32224862?s=460&v=4" alt="Gustavo Morini" />
-              <div className="user-info">
-                <strong>Gustavo Morini</strong>
-                <span>React....</span>
-              </div>
-            </header>
-            <p>bio ...</p>
-            <a href="https://github.com/gusmorini">Perfil do Github</a>
-          </li>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`}>Perfil do Github</a>
+            </li>
+          ))}
 
 
         </ul>
