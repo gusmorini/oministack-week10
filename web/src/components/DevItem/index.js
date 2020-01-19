0b330e9ba5446fc68e640917c6a33dc98f302e14
Gustavo Morini <gustavommh@gmail.com>
2020-01-19 10:14:30 -0300

    delete

---
+++ web/src/components/DevItem/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './styles.css';
+
+function DevItem({ dev, handleDeleteDev }) {
+
+    function deleteDev() {
+        handleDeleteDev(dev.github_username);
+    }
+
+    return (
+        <li className="dev-item">
+            <header>
+
+                <img
+                    src="https://cdn.icon-icons.com/icons2/931/PNG/128/delete_remove_bin_icon-icons.com_72400.png"
+                    alt="deletar"
+                    id="deletar"
+                    onClick={deleteDev}
+                />
+
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                    <strong>{dev.name}</strong>
+                    <span>{dev.techs.join(', ')}</span>
+                </div>
+
+            </header>
+
+            <p>{dev.bio}</p>
+            <a href={`https://github.com/${dev.github_username}`}>Perfil do Github</a>
+        </li>
+    );
+}
+
+export default DevItem;
